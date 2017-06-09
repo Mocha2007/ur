33be24fe5e3d3cbd588532b9f238a50e66520be8
mocha <mocha2007andcoco@gmail.com>
2017-06-08 21:06:51 -0400

    added autotranscriber

--- ur.py
+++ ur.py
@@ -6,7 +6,9 @@ blackpiece=[0,0,0,0,0,0,0]
 white=0
 black=0
 skip=0
+skipped=0
 print("Welcome to Mocha's Ur Simulator!")
+open("game.html", "a").write("<ol>\n")
 while white<7>black:
 	#white's move
 	if skip==0:
@@ -32,24 +34,44 @@ while white<7>black:
 						choice=random.choice(playablepiece)#I gave you the fucking chance and you lost it
 				whitepiece[choice]+=roll
 				#capture
+				if skipped!=1:
+					open("game.html", "a").write("\t<li>")
+				else:skipped=0
 				if (13>whitepiece[choice]>4) and (whitepiece[choice] in blackpiece):
 					for i in range(len(blackpiece)):
 						if blackpiece[i]==whitepiece[choice]:
 							blackpiece[i]=0
+					open("game.html", "a").write(str(whitepiece[choice])+"x"+str(whitepiece[choice]+roll))
+				else:
+					open("game.html", "a").write(str(whitepiece[choice])+">"+str(whitepiece[choice]+roll))
+				#forced?
+				if len(playablepiece)==1:
+					open("game.html", "a").write("f")
 				#lotus reroll
-				if whitepiece[choice]==4 or whitepiece[choice]==8 or whitepiece[choice]==14:skip=1
+				if whitepiece[choice]==4 or whitepiece[choice]==8 or whitepiece[choice]==14:
+					skip=1
+					open("game.html", "a").write("&amp;")
 				#cross the end
 				elif whitepiece[choice]==15:
 					white+=1
 					del whitepiece[choice]
+					open("game.html", "a").write("+")
 				#print
 				try:
 					print("White has moved from",whitepiece[choice]-roll,"to",whitepiece[choice],".")
 				except IndexError:
 					pass
-			else:print("You can't move!")
-		else:print("You rolled a zero!")
-	else:skip-=1
+				if skip==0:
+					open("game.html", "a").write(",")
+			else:
+				print("You can't move!")
+				open("game.html", "a").write("\t<li>&Oslash;,")
+		else:
+			print("You rolled a zero!")
+			open("game.html", "a").write("\t<li>&Oslash;,")
+	else:
+		skip-=1
+		skipped=1
 	#black's move, mirror of white's
 	if skip==0:
 		roll=dice()
@@ -76,17 +98,34 @@ while white<7>black:
 					for i in range(len(whitepiece)):
 						if whitepiece[i]==blackpiece[choice]:
 							whitepiece[i]=0
-				if blackpiece[choice]==4 or blackpiece[choice]==8 or blackpiece[choice]==14:skip=1
+					open("game.html", "a").write(str(whitepiece[choice])+"x"+str(whitepiece[choice]+roll))
+				else:
+					open("game.html", "a").write(str(whitepiece[choice])+">"+str(whitepiece[choice]+roll))
+				if len(playablepiece)==1:
+					open("game.html", "a").write("f")
+				if blackpiece[choice]==4 or blackpiece[choice]==8 or blackpiece[choice]==14:
+					skip=1
+					open("game.html", "a").write("&amp;")
 				elif blackpiece[choice]==15:
 					black+=1
 					del blackpiece[choice]
+					open("game.html", "a").write("+")
 				try:
 					print("Black has moved from",blackpiece[choice]-roll,"to",blackpiece[choice],".")
 				except IndexError:
 					pass
-			else:print("You can't move!")
-		else:print("You rolled a zero!")
-	else:skip-=1
+				if skip==0:
+					open("game.html", "a").write("</li>\n")
+			else:
+				print("You can't move!")
+				open("game.html", "a").write("&Oslash;</li>\n")
+		else:
+			print("You rolled a zero!")
+			open("game.html", "a").write("&Oslash;</li>\n")
+	else:
+		skip-=1
+		skipped=1
 if white==7:print("White wins!")
 else:print("Black wins!")
 print(white,"-",black)
+open("game.html", "a").write("</ol>\n")
